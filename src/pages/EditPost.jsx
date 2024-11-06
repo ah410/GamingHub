6a801e9abe3e1659f826521c0ec4d5578efafa0d
Ali Hamad <ah4100574@gmail.com>
2024-11-05 22:28:01 -0500

    added basic useParams functionality for editing posts

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -1,6 +1,10 @@
+import { useParams } from "react-router-dom";
+
 const EditPost = () => {
+    const { post_id } = useParams();
+
     return(
-        <h1>EditPost Page!</h1>
+        <h1>Edit Post With Id - {post_id}</h1>
     )
 }
 

1decbaa89450f660188017b87779f7c4d7d5cc51
Ali Hamad <115514687+ah410@users.noreply.github.com>
2024-11-08 21:56:55 -0500

    feat(api): add Supabase backend call to fetch posts data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRoutes } from 'react-router-dom';
+import { supabase } from '../client.js';
 import './App.css';
 
 import Home from './pages/Home';
@@ -8,11 +9,34 @@ import CreatePost from './pages/CreatePost';
 
 import Navigation from './components/Navigation';
 
-function App() {
+const App = () => {
+  // 1. Create a posts state variable to hold the posts data
+  const [posts, setPosts] = useState([]);
+
+  // 2. Create a function to fetch all the posts from the database
+  const fetchPosts = async () => {
+    const { data, error } = await supabase
+      .from('posts')
+      .select();
+
+    if (error) {
+      console.log("Error fetching posts: ", error);
+    } else {
+      setPosts(data);
+      console.log("Success fetching posts: ", data);
+    }
+  }
+
+  // 3. useEffect hook to fetch the posts data on first render
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+
   const routes = useRoutes([
     {
       path: '/',
-      element: <Home />
+      element: <Home posts={posts} />
     },
     {
       path: '/create_post',

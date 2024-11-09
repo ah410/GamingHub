ac45bc2a2277abf495762a9aab61596793c7798d
Ali Hamad <115514687+ah410@users.noreply.github.com>
2024-11-08 21:58:38 -0500

    feat(posts): display posts data on Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,19 @@
-const Home = () => {
+const Home = ({posts}) => {
     return(
-        <h1>Home Page!</h1>
+        <div>
+            <h1>Home Page!</h1>
+            {posts && posts.map((post) => {
+                return (
+                    <div key={post.id} className="flex flex-col mb-5">
+                        <h2>Title: {post.title}</h2>
+                        <p>Description: {post.description}</p>
+                        <p>Upvotes: {post.upvotes}</p>
+                        <p>Downvotes: {post.downvotes}</p>
+                        <p>Comments: {post.comments}</p>
+                    </div>
+                )
+            })}
+        </div>
     )
 }
 

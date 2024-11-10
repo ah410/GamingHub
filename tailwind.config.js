3f5989e311108d94a36c3db57f99b1bc932e9934
Ali Hamad <115514687+ah410@users.noreply.github.com>
2024-11-09 21:41:24 -0500

    style(tailwind): added dark primary color for hover effects in tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ export default {
     extend: {
       colors: {
         'primary': '#1E3A8A',
+        'primary-dark': '#172c6a',
         'secondary': '#F97316',
         'background': '#242424',
         'card-background': '#374151'

01429f138448bf1a40fc28c2d2f657e52cbe997b
Ali Hamad <115514687+ah410@users.noreply.github.com>
2024-11-15 23:15:20 -0500

    style: added light variant of primary color for button highlights

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ export default {
       colors: {
         'primary': '#1E3A8A',
         'primary-dark': '#172c6a',
+        'primary-light': '#3B6BBF',
         'secondary': '#F97316',
         'background': '#242424',
         'background-hover': '#2e2e2e',

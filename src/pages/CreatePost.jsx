00d115e7ce38415ed51e67a79558cf90742c02c1
Ali Hamad <115514687+ah410@users.noreply.github.com>
2024-11-20 00:04:29 -0500

    refactor: switched layout of page and styling

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -57,20 +57,27 @@ const CreatePost = () => {
 
 
     return(
-        <div className='flex flex-col justify-between my-10'>
+        <div className='flex flex-col justify-between my-10 text-left w-5/6 lg:w-1/2 2xl:w-1/3 max-w-5xl'>
             <h1>Create New Post</h1>
-            <form className='max-w-sm mx-auto w-full mt-10'>
-                <div className="title-div mb-5">
-                    <label htmlFor="title" className='block'>Title:</label>
-                    <input type="text" className='mt-1 text-background rounded-md' required id="title" name="title" onChange={handleChange}/>
+            <form className='mx-auto w-full mt-10'>
+                <div className="title-div mb-5 w-full">
+                    <label htmlFor="title" className='block'>Title</label>
+                    <input type="text" className='mt-1 bg-background-card rounded-md w-full' required id="title" name="title" onChange={handleChange}/>
                 </div>
 
-                <div className="description-div mb-5">
-                    <label htmlFor="description" className='block'>Description:</label>
-                    <textarea rows={5} type="text" className='mt-1 text-background w-full rounded-md' id="description" name="description" onChange={handleChange}/>
+                <div className="description-div mb-5 w-full">
+                    <label htmlFor="description" className='block'>Content</label>
+                    <textarea rows={5} type="text" className='mt-1 bg-background-card w-full rounded-md' id="description" name="description" onChange={handleChange}/>
                 </div>
 
-                <button type='submit' onClick={handleSubmit} className="bg-primary py-2 px-5 rounded-lg hover:bg-primary-dark">Post</button>
+                <div className='w-full flex justify-end'>
+                    <button type='submit' onClick={handleSubmit} className="bg-primary py-2 px-5 rounded-lg hover:bg-primary-dark flex items-center justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5 mr-1">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+                        </svg>
+                        <span className='ml-1'>Create Post</span>
+                    </button>
+                </div>
             </form>
         </div>
     )
--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -31,6 +31,7 @@ const EditPost = () => {
             console.log("Success fetching post: ", data);
         }
         setPostDatabase(data[0]);
+        setPostDetails({title: data[0].title, description: data[0].description})
     }
 
     useEffect(() => {
@@ -72,7 +73,9 @@ const EditPost = () => {
     }
 
 
-    const deletePost = async () => {
+    const deletePost = async (e) => {
+        e.preventDefault();
+        
         const { data, error } = await supabase
             .from('posts')
             .delete()
@@ -89,22 +92,28 @@ const EditPost = () => {
     }
 
     return(
-        <div className="my-10">
-            <h1>Edit Post: {postDatabase && postDatabase.title}</h1>
-            <form className='max-w-sm mx-auto mt-10'>
-                <div className="title-div mb-5">
-                    <label htmlFor="title" className='block'>Title:</label>
-                    <input type="text" className='text-background mt-1' required id="title" name="title" onChange={handleChange}/>
+        <div className="flex flex-col justify-between my-10 text-left w-5/6 lg:w-1/2 2xl:w-1/3 max-w-5xl">
+            <h1>Edit Post</h1>
+            <form className='mx-auto w-full mt-10'>
+                <div className="title-div mb-5 w-full">
+                    <label htmlFor="title" className='block'>Title</label>
+                    <input type="text" className='mt-1 bg-background-card rounded-md w-full' required id="title" name="title" value={postDetails.title} onChange={handleChange}/>
                 </div>
 
                 <div className="description-div mb-5">
-                    <label htmlFor="description" className='block'>Description:</label>
-                    <textarea rows={5} type="text" className='text-background w-full mt-1' id="description" name="description" onChange={handleChange}/>
+                    <label htmlFor="description" className='block'>Description</label>
+                    <textarea rows={5} type="text" className='mt-1 bg-background-card rounded-md w-full' id="description" name="description" value={postDetails.description} onChange={handleChange}/>
                 </div>
 
-                <div className="flex items-center justify-center">
-                    <button type='submit' onClick={handleSubmit} className="bg-primary p-3 m-2 rounded-lg hover:bg-primary-dark">Update Post</button>
-                    <button type='submit' onClick={deletePost} className="bg-accent p-3 m-2 rounded-lg hover:bg-accent-dark">Delete Post</button>
+                <div className="flex items-center justify-end">
+                    <button type='submit' onClick={deletePost} className="bg-accent p-3 mr-4 rounded-lg hover:bg-accent-dark flex items-center justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash2 w-4 h-4 mr-2"><path d="M3 6h18"></path><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"></path><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path><line x1="10" x2="10" y1="11" y2="17"></line><line x1="14" x2="14" y1="11" y2="17"></line></svg>
+                        <span>Delete</span>
+                    </button>
+                    <button type='submit' onClick={handleSubmit} className="bg-primary p-3 rounded-lg hover:bg-primary-dark flex items-center justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-save w-4 h-4 mr-2"><path d="M15.2 3a2 2 0 0 1 1.4.6l3.8 3.8a2 2 0 0 1 .6 1.4V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2z"></path><path d="M17 21v-7a1 1 0 0 0-1-1H8a1 1 0 0 0-1 1v7"></path><path d="M7 3v4a1 1 0 0 0 1 1h7"></path></svg>
+                        <span>Save Changes</span>
+                    </button>
                 </div>
             </form>
         </div>

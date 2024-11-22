96735aeb6cec7446e357f6d7f559dffa25bc16ed
Ali Hamad <115514687+ah410@users.noreply.github.com>
2024-11-21 23:19:25 -0500

    refactor(Navigation): moved home link and increased search bar width

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import mario from '../assets/mario.gif';
+import GameController from "./GameController";
 
 const Navigation = ({ posts, setFilteredPosts, setSearchValue, setSortedPosts }) => {
     // 1. Make a function to filter
@@ -24,13 +25,17 @@ const Navigation = ({ posts, setFilteredPosts, setSearchValue, setSortedPosts })
         <nav className="bg-primary flex justify-center items-center w-screen">
             <ul className="flex justify-between w-full">
                 <div className="flex items-center justify-center">
-                    <li className="flex justify-center items-center p-4">👾 GameHub 👾</li>
-                    <img src={mario} alt="" width={90} className="rounded-lg p-1" />
+                    <li className="flex justify-center items-center pl-4">
+                        <Link to='/' className="flex justify-center items-center">
+                            <GameController/>
+                            <div className="text-md sm:text-2xl font-medium">GameHub</div>
+                        </Link>
+                    </li>
                 </div>
 
-                <li className="flex justify-center items-center p-4">
+                <li className="flex justify-center items-center p-4 w-1/3">
                     <input 
-                        className="rounded-full bg-background focus:outline-none focus:border-secondary focus:ring-2 focus:ring-secondary" 
+                        className="rounded-full bg-background focus:outline-none focus:border-secondary focus:ring-2 focus:ring-secondary w-full" 
                         type="text" 
                         placeholder="Search..."
                         onChange={(inputValue) => searchPosts(inputValue.target.value)}
@@ -39,11 +44,7 @@ const Navigation = ({ posts, setFilteredPosts, setSearchValue, setSortedPosts })
                 
                 <div className="flex items-center justify-center p-4">
                     <li className="flex justify-center items-center">
-                        <Link to='/' className="px-4 py-2 hover:bg-secondary rounded-full">Home</Link>
-                    </li>
-                    
-                    <li className="flex justify-center items-center">
-                        <Link to='/create_post' className="flex bg-secondary hover:bg-secondary-dark rounded-lg px-3 py-2 m-2">
+                        <Link to='/create_post' className="flex items-center justify-center bg-secondary hover:bg-secondary-dark rounded-lg px-3 py-2 m-2">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 mr-1">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                             </svg>

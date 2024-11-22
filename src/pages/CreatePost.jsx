9a6ce4c0bcb8d7c85e08a0f381b4866ced9f77ae
Ali Hamad <115514687+ah410@users.noreply.github.com>
2024-11-21 23:31:02 -0500

    refactor(CreatePost): changed tags to one tag only for each post and attributes of svg to react attributes

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -3,7 +3,7 @@ import { supabase } from '../../client';
 
 const CreatePost = () => {
     // 1. Create a posts state variable to hold the posts data
-    const [post, setPost] = useState({title: '', description: '', upvotes: 0, downvotes: 0, comments: 0, tags:[]});
+    const [post, setPost] = useState({title: '', description: '', upvotes: 0, downvotes: 0, comments: 0, tag: ''});
     const [tagsAreActive, setTagsAreActive] = useState({'Guide': false, 'Walkthrough': false, 'Tips': false, 'Discussion': false, 'Updates': false});
     const tags = ['Guide', 'Walkthrough', 'Tips', 'Discussion', 'Updates'];
 
@@ -50,17 +50,28 @@ const CreatePost = () => {
     const handleTagSelection = (e, tag) => {
         e.preventDefault();
 
-        setTagsAreActive({...tagsAreActive, [tag]: !tagsAreActive[tag]});   // ES6: Computed Property Names. [varName] to grab string literal of variable
-
-        // Add tag to post.tags using spread operator
-        setPost((prevState) => {
-            return {...prevState, tags: [...prevState.tags, tag]};
+        // Check if any of the tags are active, if so return, if not, set the tag to active
+        const tagActive = {};
+        Object.keys(tagsAreActive).map((tag) => {
+            if (tagsAreActive[tag]) {
+                tagActive[tag] = true;
+            } 
         });
+
+        // If there is an active tag AND the active tag isn't the one clicked on, return. Else, set the clicked tag to active and add it to the post
+        if (Object.values(tagActive)[0] && Object.keys(tagActive)[0] != tag) {
+            return;
+        } else {
+            setTagsAreActive({...tagsAreActive, [tag]: !tagsAreActive[tag]});   // ES6: Computed Property Names. [varName] to grab string literal of variable
+            setPost((prevState) => {
+                return {...prevState, tag: tag};
+            });
+        }
     }
 
 
     return(
-        <div className='flex flex-col justify-between my-10 text-left w-5/6 lg:w-1/2 2xl:w-1/3 max-w-5xl'>
+        <div className='flex flex-col justify-between my-10 text-left w-11/12 sm:w-5/6 lg:w-1/2 2xl:w-1/3 max-w-5xl'>
             <h1 className='text-3xl font-bold'>Create New Post</h1>
             <form className='mx-auto w-full mt-10'>
                 <div className="title-div mb-5 w-full">
@@ -69,31 +80,31 @@ const CreatePost = () => {
                 </div>
 
                 <div className='flex flex-col justify-center mb-5'>
-                <span>Post Category</span>
-                <div className='flex mt-2 gap-2'>
-                    {tags.map((tag) => {
-                        return (
-                            <button onClick={(event) => handleTagSelection(event,tag)} className={`flex ${tagsAreActive[tag] ? 'bg-secondary-dark' : 'bg-gray-700'} px-3 py-1 rounded-full items-center justify-center font-medium hover:bg-secondary transition-colors duration-200`}>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 mr-1">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z" />
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6Z" />
-                                </svg>
-                                {tag}
-                            </button>
-                        )
-                    })}
-                </div>
+                    <span>Post Category</span>
+                    <div className='flex flex-wrap mt-2 gap-2'>
+                        {tags.map((tag, index) => {
+                            return (
+                                <button key={index} onClick={(event) => handleTagSelection(event,tag)} className={`flex ${tagsAreActive[tag] ? 'bg-secondary-dark' : 'bg-gray-700'} px-3 py-1 rounded-full items-center justify-center font-medium hover:bg-secondary transition-colors duration-200`}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5 mr-1">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z" />
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 6h.008v.008H6V6Z" />
+                                    </svg>
+                                    {tag}
+                                </button>
+                            )
+                        })}
+                    </div>
                 </div>
 
                 <div className="description-div mb-5 w-full">
                     <label htmlFor="description" className='block'>Content</label>
-                    <textarea rows={5} type="text" className='mt-1 bg-background-card w-full rounded-md' id="description" name="description" onChange={handleChange}/>
+                    <textarea rows={7} type="text" className='mt-1 bg-background-card w-full rounded-md' id="description" name="description" onChange={handleChange}/>
                 </div>
 
                 <div className='w-full flex justify-end'>
                     <button type='submit' onClick={handleSubmit} className="bg-primary py-2 px-5 rounded-lg hover:bg-primary-dark flex items-center justify-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5 mr-1">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5 mr-1">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
                         </svg>
                         <span className='ml-1'>Create Post</span>
                     </button>

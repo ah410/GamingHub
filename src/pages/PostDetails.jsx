4e1d69a112e1d53afd3a4a1500ebcd7019cfa3f1
Ali Hamad <115514687+ah410@users.noreply.github.com>
2025-01-11 16:16:59 -0500

    style(post details): color change on hover for div instead of icon for like, dislike, and comment button

--- src/pages/PostDetails.jsx
+++ src/pages/PostDetails.jsx
@@ -140,18 +140,18 @@ const PostDetails = ({ setPosts }) => {
 
                 <div className="stats flex p-4">
                     <div className="likes flex mr-3 bg-background p-2 rounded-full">
-                        <span className="mr-2 flex items-center hover:bg-background-hover p-1 h-full rounded-full" onClick={updateUpvotes}>
+                        <span className="mr-2 flex items-center hover:bg-background-hover hover:text-secondary p-1 h-full rounded-full" onClick={updateUpvotes}>
                             <span className="mr-1">
-                                <svg className="hover:stroke-secondary" rpl="" fill="currentColor" height="16" icon-name="upvote-outline" viewBox="0 0 20 20" width="16" xmlns="http://www.w3.org/2000/svg">
+                                <svg rpl="" fill="currentColor" height="16" icon-name="upvote-outline" viewBox="0 0 20 20" width="16" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M10 19c-.072 0-.145 0-.218-.006A4.1 4.1 0 0 1 6 14.816V11H2.862a1.751 1.751 0 0 1-1.234-2.993L9.41.28a.836.836 0 0 1 1.18 0l7.782 7.727A1.751 1.751 0 0 1 17.139 11H14v3.882a4.134 4.134 0 0 1-.854 2.592A3.99 3.99 0 0 1 10 19Zm0-17.193L2.685 9.071a.251.251 0 0 0 .177.429H7.5v5.316A2.63 2.63 0 0 0 9.864 17.5a2.441 2.441 0 0 0 1.856-.682A2.478 2.478 0 0 0 12.5 15V9.5h4.639a.25.25 0 0 0 .176-.429L10 1.807Z"></path>
                                 </svg>
                             </span>
                             <span>{post && post.upvotes}</span>
                         </span>
 
-                        <span className="flex items-center hover:bg-background-hover p-1 h-full rounded-full" onClick={updateDownvotes}>
+                        <span className="flex items-center hover:bg-background-hover hover:text-primary-light2 p-1 h-full rounded-full" onClick={updateDownvotes}>
                             <span className="mr-1">
-                                <svg className="hover:stroke-secondary" rpl="" fill="currentColor" height="16" icon-name="downvote-outline" viewBox="0 0 20 20" width="16" xmlns="http://www.w3.org/2000/svg">
+                                <svg rpl="" fill="currentColor" height="16" icon-name="downvote-outline" viewBox="0 0 20 20" width="16" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M10 1c.072 0 .145 0 .218.006A4.1 4.1 0 0 1 14 5.184V9h3.138a1.751 1.751 0 0 1 1.234 2.993L10.59 19.72a.836.836 0 0 1-1.18 0l-7.782-7.727A1.751 1.751 0 0 1 2.861 9H6V5.118a4.134 4.134 0 0 1 .854-2.592A3.99 3.99 0 0 1 10 1Zm0 17.193 7.315-7.264a.251.251 0 0 0-.177-.429H12.5V5.184A2.631 2.631 0 0 0 10.136 2.5a2.441 2.441 0 0 0-1.856.682A2.478 2.478 0 0 0 7.5 5v5.5H2.861a.251.251 0 0 0-.176.429L10 18.193Z"></path>
                                 </svg>
                             </span>
@@ -160,8 +160,8 @@ const PostDetails = ({ setPosts }) => {
 
                     </div>
 
-                    <div className="comments-div flex items-center bg-background p-2 rounded-full hover:bg-background-hover">
-                        <span><ChatBubbleOvalLeftIcon className="size-5 mr-1 hover:stroke-secondary"/></span>
+                    <div className="comments-div flex items-center bg-background p-2 rounded-full hover:bg-background-hover hover:text-secondary">
+                        <span><ChatBubbleOvalLeftIcon className="size-5 mr-1"/></span>
                         <span>{post && post.comments}</span>
                     </div>
                 </div>
